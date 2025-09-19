caf0be6f6be213a92aa46cde8fa25d375165f586
yhernandez <32073319+yusel-hernandez@users.noreply.github.com>
2025-09-19 23:32:43 +0200

    ch3-l1

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from stats import get_num_words, get_num_chars
+from stats import get_num_words, get_num_chars, sorted_list
 
 BOOKPATH = "./books/frankenstein.txt"
+
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -8,10 +9,24 @@ def get_book_text(filepath):
         return bookstr
 
 
-def main():
+def print_report():
     book_text = get_book_text(BOOKPATH)
-    print(f"{get_num_words(book_text)} words found in the document")
-    print(get_num_chars(book_text))
+    chars_dict = get_num_chars(book_text)
+    count_ordered_list = sorted_list(chars_dict)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {BOOKPATH}...")
+    print("----------- Word Count ----------")
+    print(f"Found {get_num_words(book_text)} total words")
+    print("--------- Character Count -------")
+    for item in count_ordered_list:
+        if item["letter"].isalpha():
+            print(f"{item['letter']}: {item['quantity']}")
+
+
+def main():
+    print_report()
     return
+
 
 main()
--- stats.py
+++ stats.py
@@ -1,9 +1,22 @@
 def get_num_words(string):
     return len(string.split())
 
+
 def get_num_chars(string):
     result_dict = {}
     for char in string.lower():
         num = result_dict.get(char, 0)
-        result_dict.update({char: (num+1)})
+        result_dict.update({char: (num + 1)})
     return result_dict
+
+
+def sort_on(items):
+    return items["quantity"]
+
+
+def sorted_list(char_dict):
+    sorted = []
+    for key, value in char_dict.items():
+        sorted.append({"letter": key, "quantity": value})
+    sorted.sort(reverse=True, key=lambda item: item['quantity'])
+    return sorted
